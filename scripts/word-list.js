3c8a435e62beb3dc769a6f334a7d54c8bd76b5c7
Mirabella-design <Mirabella.marubio@gmail.com>
2024-09-03 09:30:29 -0500

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,103 @@
+const wordList = [
+    {
+        word: "aestas",
+        hint: "The hottest time of the year."
+    },
+    {
+        word: "accubo",
+        hint: "What you do at dinner."
+    },
+    {
+        word: "bellum",
+        hint: "Something the romans engaged in frequently."
+    },
+    {
+        word: "caput",
+        hint: "Try to think with this."
+    },
+    {
+        word: "corona",
+        hint: "A king would have it."
+    },
+    {
+        word: "crinis",
+        hint: "It grows on you."
+    },
+    {
+        word: "dux",
+        hint: "Someone who has followers."
+    },
+    {
+        word: "fabula",
+        hint: "Something told in a taberna maybe."
+    },
+    {
+        word: "fortuna",
+        hint: "What you may inherit."
+    },
+    {
+        word: "hodie",
+        hint: "The present."
+    },
+    {
+        word: "ianua",
+        hint: "The first month of the year is related to it."
+    },
+    {
+        word: "ignis",
+        hint: "Something you make while camping."
+    },
+    {
+        word: "iudex",
+        hint: "Someone who runs a court."
+    },
+    {
+        word: "legio",
+        hint: "Augustus inherited these from Julius Caesar."
+    },
+    {
+        word: "ludus",
+        hint: "Something you play."
+    },
+    {
+        word: "mane",
+        hint: "When the sun rises."
+    },
+    {
+        word: "miles",
+        hint: "They are needed in an army."
+    },
+    {
+        word: "novem",
+        hint: "3²."
+    },
+    {
+        word: "pipio",
+        hint: "Something that birds do."
+    },
+    {
+        word: "pugno",
+        hint: "What you do in war."
+    },
+    {
+        word: "prope",
+        hint: "Close to."
+    },
+    {
+        word: "respondeo",
+        hint: "What you do when a question is asked."
+    },
+    {
+        word: "surgo",
+        hint: "What the sun does each morning."
+    },
+    {
+        word: "ventus",
+        hint: "What may accompany a stormy day."
+    },
+    {
+        word: "viridis",
+        hint: "Grass, frogs, broccoli."
+    },
+
+];
